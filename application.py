336810dbb04e2580cb9d9ae0e005a9c5496b6098
emilylaiken <emilyaiken@ollege.harvard.edu>
2017-10-10 23:24:24 -0400

    added starting files

---
+++ application.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, redirect, render_template, request, url_for, send_file, session
+import os
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/map")
+def map():
+	# read sites CSV into pandas dataframe
+	sites = pd.DataFrame()
+	# read cases CSV into pandas dataframe
+	cases = pd.DataFrame()
+	# pass two dataframes to front end
+	return render_template("map.html", sites=sites.to_html(), cases=cases.to_html())
+
+# Run application
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
