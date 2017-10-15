5f14216c45592aab1a83c726b8800e093cc427b2
Allison Lee <allison_lee@college.harvard.edu>
2017-10-15 19:06:49 -0400

    Finished GetFromComm -Allison

--- helpers.py
+++ helpers.py
@@ -1,3 +1,15 @@
 import pandas as pd
 from flask import redirect, render_template, request, session, url_for
+import sys
 
+# read sites CSV into pandas dataframe
+sites = pd.read_csv('data/sites.csv')
+cases = pd.read_csv('data/cases.csv')
+output = pd.read_csv('data/output.csv')
+
+def getFromCommunity(city, state): 
+	rightCity = sites["city"] == city
+	rightState = sites["state"] == state
+	dic = sites[rightCity & rightState].to_dict(orient="index")[0]
+	print(dic, file=sys.stderr)
+	return dic
