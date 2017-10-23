1deb672784479e57c772972d38c9d175482a26ae
wanqianxn <ywanqian95@gmail.com>
2017-10-23 00:43:34 -0400

    more backend functions

--- helpers.py
+++ helpers.py
@@ -7,17 +7,39 @@ sites = pd.read_csv('data/sites.csv')
 cases = pd.read_csv('data/cases.csv')
 output = pd.read_csv('data/output.csv')
 
-# Takes refugee group's name and returns dict of relevant info
+# Takes refugee group's name and returns dict of relevant info.
 def refugeesInfo(groupName):
-    languages = cases[cases["name"] == groupName].iloc[0]["languages"]
-    refugeesInfo = cases[cases["name"] == groupName].iloc[0].to_dict()
-    refugeesInfo["languages"] = languages.replace(" ", "").split(",")
+    refDf = cases[cases['name'] == groupName]
+    if refDf.empty:
+        return None
+    refugeesInfo = refDf.iloc[0].to_dict()
+    languages = refDf.iloc[0]['languages']
+    refugeesInfo['languages'] = languages.replace(" ", "").split(",")
+    for i in range(len(output.index)):
+        if output.iloc[i, 0] == groupName:
+            for j in range(len(output.columns)):
+                if output.iloc[i, j] == 1:
+                    refugeesInfo['community'] = output.columns[j]
     return refugeesInfo
 
-# Takes community name and returns dict of relevant info
-def getFromCommunity(city, state):
-	rightCity = sites["city"] == city
-	rightState = sites["state"] == state
-	dic = sites[rightCity & rightState].to_dict(orient="index")[0]
-	print(dic, file=sys.stderr)
-	return dic
+# Takes community's details (city, state) and returns dict of relevant info.
+def communityInfo(city, state):
+    comDf = sites[(sites['city'] == city) & (sites['state'] == state)]
+    if comDf.empty:
+        return None
+    return comDf.iloc[0].to_dict()
+
+# Return all communities in a dict list.
+def listOfCommunities():
+    comList = []
+    for i in range(len(sites.index)):
+        comList.append(communityInfo(sites.iloc[i]['city'],sites.iloc[i]['state']))
+    return comList
+
+# Return all refugees from a particular community in a dict list.
+def listOfRefugees(city):
+    refList = []
+    for name in cases['name']:
+        if refugeesInfo(name)['community'] == city:
+            refList.append(refugeesInfo(name))
+    return refList
